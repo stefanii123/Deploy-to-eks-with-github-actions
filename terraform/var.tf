9dc45d9120faf1273d27bf417e103943402b7a7d
Ose Imoisili <oseimoisili@gmail.com>
2023-03-20 04:30:50 +0100

    Edits

--- terraform/var.tf
+++ terraform/var.tf
@@ -1,14 +1,5 @@
-#var.tf file
-
-variable "kubeconfig_path" {
-  description = "Path to Kubernetes config file"
-}
-
-variable "namespace" {
-  description = "Namespace where the resources will be created"
-}
-
-variable "manifests" {
-  description = "List of Kubernetes manifests to apply"
-  type = list(string)
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
